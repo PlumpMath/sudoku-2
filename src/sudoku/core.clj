0b0d19401e7d6b5597fef1221907e0f3a891e68d
Erik Svedäng <erik.svedang@gmail.com>
2012-12-11 20:11:10 +0100

    greenbar

--- src/sudoku/core.clj
+++ src/sudoku/core.clj
@@ -1,19 +1,5 @@
 (ns sudoku.core)
 
-(def puzzle1 [1 2 3 4
-              3 4 1 2
-              4 1 2 3
-              2 3 4 1])
-
-(defn all-unique? [row]
-  (= (count (set row))
-     (count row)))
-
-(defn is-solved?
-  "Takes a solved puzzle and returns true if the puzzle is solved"
-  [puzzle]
-  false)
-
 (defn get-width [puzzle]
   (Math/sqrt (count puzzle)))
 
@@ -30,6 +16,41 @@
 	  (for [start (range 0 width)]
 	    (for [i (range start (* width width) width)]
 	    	(nth puzzle i)))))
+
+(defn get-sector-indexes
+  [puzzle]
+  (let [puzzle-width (get-width puzzle)
+        sector-width (int (Math/sqrt puzzle-width))]
+    (for [sector-x (range 0 sector-width)
+          sector-y (range 0 sector-width)]
+      (for [inside-x (range 0 sector-width)
+            inside-y (range 0 sector-width)]
+	      (let [x (+ inside-x (* sector-x sector-width))
+                 y (+ inside-y (* sector-y sector-width))
+                 w puzzle-width]
+     	   (+ x (* y w)))))))
+
+(defn get-sectors
+  [puzzle]
+  (for [coordinates (get-sector-indexes puzzle)]
+    (for [coord coordinates]
+      (nth puzzle coord))))
+
+(defn all-unique? [row]
+  (= (count (set row))
+     (count row)))
+
+(defn all-true?
+  [f puzzle]
+  (reduce #(and %1 %2) (map all-unique? (f puzzle))))
+
+(defn is-solved?
+  "Takes a solved puzzle and returns true if the puzzle is solved"
+  [puzzle]
+  (and
+   (all-true? get-rows puzzle)
+   (all-true? get-columns puzzle)
+   (all-true? get-sectors puzzle)))
 
 (defn solve 
   "Takes an unsolved puzzle and returns a solved one"
--- test/sudoku/core_test.clj
+++ test/sudoku/core_test.clj
@@ -2,6 +2,38 @@
   (:use clojure.test
         sudoku.core))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def puzzle1 [1 2 3 4
+              3 4 1 2
+              4 1 2 3
+              2 3 4 1])
+
+(def puzzle2 [2 1 3 4
+              3 4 1 2
+              4 1 2 3
+              2 3 4 1])
+
+(def puzzle3 [1 2 3 4
+              3 4 1 2
+              4 1 4 3
+              2 3 2 1])
+
+(def puzzle4 [1 2 3 4
+              4 3 1 2
+              3 1 2 4
+              2 4 3 1])
+
+(deftest solving-test-1
+  (testing ""
+    (is (is-solved? puzzle1))))
+
+(deftest solving-test-2
+  (testing ""
+    (is (not (is-solved? puzzle2)))))
+
+(deftest solving-test-3
+  (testing ""
+    (is (not (is-solved? puzzle3)))))
+
+(deftest solving-test-4
+  (testing ""
+    (is (not (is-solved? puzzle4)))))
